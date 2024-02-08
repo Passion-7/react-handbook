1326882dcbf7ff3e15e6b015571bc5a5f27353cd
passion <oscarpassion36@gmail.com>
2024-02-08 23:02:39 +0800

    Add react-buddy

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import styled from "styled-components";
 
 function App() {
-  return (
-    <div>
-      <p>Start Hacking!</p>
-    </div>
-  );
+    return (
+        <div>
+            <p>Start Hacking!</p>
+        </div>
+    );
 }
 
 export default App;
