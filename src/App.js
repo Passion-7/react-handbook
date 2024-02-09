6a35cde0d6253b122c6f26f8e892bd4e198ca6bb
passion <oscarpassion36@gmail.com>
2024-02-09 22:51:44 +0800

    Base template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import './App.css';
 import styled from "styled-components";
 
+import backgroundImage from "./images/background.png";
+
 function App() {
     return (
-        <div>
-            <p>Start Hacking!</p>
-        </div>
+        <Wrapper>
+            <Background src={backgroundImage} alt="background" />
+            <Title>useState</Title>
+        </Wrapper>
     );
 }
+
+const Wrapper = styled.div`
+    padding-top: 150px;
+    margin: 0 auto;
+    display: grid;
+    justify-items: center;
+`
+
+const Title = styled.h1`
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    color: white;
+    line-height: 48px;
+    font-size: 40px;
+    text-align: center;
+`
+
+const Description = styled.h2`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 32px;
+  line-height: 48px;
+  color: #ffffff;
+  text-align: center;
+`;
+
+const Background = styled.img`
+    top: 0;
+    position: absolute;
+    width: 100%;
+    z-index: -1;
+`
 
 export default App;
